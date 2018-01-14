bf318ae052ea18c889b58d131b756100ae9beadf
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-13 17:52:20 -0800

    Add tasks to firebase and then listen and pull new tasks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,18 +17,83 @@ function connectDatabase(){
 }
 
 
-function createTask(text){
+function createTask(text,completed){
 
 	const Task = {
 		description: text,
-		completed: false,
-		ownerid: undefined,
-	
+		completed: completed,
+		taskId: undefined,
+		ownerId:undefined,
 	}
 
-	//push task to database
 
 	return Task;
+}
+
+function addTaskToDatabase(task, ownerId, Database){
+
+	const taskRef = Database.ref('tasks');
+	const key = taskRef.push().key;
+	task.taskId = key;
+	task.ownerId = ownerId;
+	taskRef.push(task);
+}
+
+function addUserToDatabase(user, Database){
+
+	const userRef = Database.ref('users');
+	const key = userRef.push().key;
+	user.id = key;
+	userRef.push(key);
+
+}
+
+function createUser(){
+
+	const User = {
+		id:undefined
+	}
+
+	return User;
+}
+
+
+
+function getCompletedTasks(Database){
+
+	const taskRef = Database.ref('tasks');
+	const PageHandler = createPageHandler();
+	PageHandler.clearCompleteTasks();
+	taskRef.orderByChild("completed").equalTo(true).once("value",function(snapshot){
+
+		for(let key in snapshot.val()){
+
+			PageHandler.addCompleteTask(createTask(snapshot.val()[key].description, snapshot.val()[key].completed));
+
+		}
+
+	});
+
+
+}
+
+
+function getIncompleteTasks(Database){
+
+	const taskRef = Database.ref('tasks');
+	const PageHandler = createPageHandler();
+	PageHandler.clearIncompleteTasks();
+	taskRef.orderByChild("completed").equalTo(false).once("value",function(snapshot){
+
+		for(let key in snapshot.val()){
+
+			PageHandler.addIncompleteTask(createTask(snapshot.val()[key].description, snapshot.val()[key].completed));
+
+		}
+
+	});
+
+
 }
 
 
@@ -42,17 +107,39 @@ function createPageHandler(){
 		completedTasks : [],
 		incompleteTasks: [],
 
+		clearCompleteTasks : function(){
+
+			this.completedTaskContainer.empty();
+		},
+		clearIncompleteTasks : function(){
+
+			this.incompleteTaskContainer.empty();
+		},
+
 		addIncompleteTask : function(task){
 
-			let taskElement = "<div class=\"task\">";
+			let taskElement = "<div class=\"task\" taskid=\"" + task.taskid + "\">";
 			taskElement += "<div class=\"task-text\">" + task.description + "</div>";
 			taskElement += "<button type=\"button\" class=\"btn btn-success complete-btn\">";
 			taskElement += "Complete" + "</button>";
 			taskElement += "<button type=\"button\" class=\"btn btn-danger delete-btn\">";
 			taskElement += "Delete" + "</button>";
-			//create task element
+			
 			this.incompleteTaskContainer.append(taskElement);
+			
 		},
+		addCompleteTask : function(task){
+
+			let taskElement = "<div class=\"task\" taskid=\"" + task.taskid + "\">";
+			taskElement += "<div class=\"task-text\">" + task.description + "</div>";
+			taskElement += "<button type=\"button\" class=\"btn btn-warning incomplete-btn\">";
+			taskElement += "Incomplete" + "</button>";
+			taskElement += "<button type=\"button\" class=\"btn btn-danger delete-btn\">";
+			taskElement += "Delete" + "</button>";
+			
+			this.completedTaskContainer.append(taskElement);
+			
+		}
 
 	}
 
@@ -61,16 +148,41 @@ function createPageHandler(){
 
 }
 
+function createDatabaseListeners(Database){
+
+	const userRef = Database.ref('users');
+	const taskRef = Database.ref('tasks');
+
+
+	taskRef.on("value", function(){
+
+		getIncompleteTasks(Database);
+		getCompletedTasks(Database);
+
+
+	});
+	
+
+
+}
+
 $(document).ready(function(){
 
 	const Database = connectDatabase();
 	const PageHandler = createPageHandler();
+	const User = createUser();
 
+	addUserToDatabase(User,Database);
+	createDatabaseListeners(Database);
 
 	$(document).on('click', '.new-task-btn', function(){
 
-		const task = createTask("hello there!");
-		PageHandler.addIncompleteTask(task);
+		const task = createTask("hello there!", false);
+
+		addTaskToDatabase(task,User.id,Database);
+		
+		
+		
 
 
 	});
